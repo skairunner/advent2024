7f77d0660d2af091cec4e32f7b898efc82a56f4c
Skye Im <fluffyterran@gmail.com>
2024-12-06 20:28:21 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -21,63 +21,40 @@ fn read_input() -> (Vec<i64>, Vec<i64>) {
     (list1, list2)
 }
 
-struct Number {
-    pub n: i64,
-    pub pos: usize,
-}
-
-impl PartialOrd for Number {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let o = self.n.partial_cmp(&other.n);
-        match o {
-            Some(Ordering::Equal) => self.pos.partial_cmp(&other.pos),
-            other => other,
-        }
-    }
-}
-
-impl Eq for Number {}
-
-// bit of a funky partialeq implementation but i'm lazy
-impl PartialEq<Self> for Number {
-    fn eq(&self, other: &Self) -> bool {
-        self.n == other.n
-    }
-}
-
-impl Ord for Number {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.n.cmp(&other.n)
-    }
-}
-
-/// Transform lists into pairs of (number, position)
-fn transform_list(list: &[i64]) -> Vec<Number> {
-    list.iter()
-        .enumerate()
-        .map(|(pos, n)| Number { pos, n: *n })
-        .collect()
-}
 
 /// Do the operation in the problem description
-fn match_numbers(list1: &[i64], list2: &[i64]) -> usize {
-    // First, parse the list numbers into pairs of (number, smallest position)
-    let mut list1 = transform_list(list1);
-    let mut list2 = transform_list(list2);
+fn match_numbers(mut list1: Vec<i64>, mut list2: Vec<i64>) -> u64 {
     list1.sort();
     list2.sort();
 
     // Now zip the two and calculate scores.
     list1
-        .iter()
-        .zip(&list2)
-        .map(|(n1, n2)| n1.pos.abs_diff(n2.pos))
+        .into_iter()
+        .zip(list2)
+        .map(|(n1, n2)| n1.abs_diff(n2))
+        .sum()
+}
+
+fn calculate_similarity(list1: Vec<i64>, list2: Vec<i64>) -> usize {
+    // Transform list2 into a count of numbers
+    let mut numbers: HashMap<i64, usize> = HashMap::new();
+    for n in list2 {
+        if let Some(count) = numbers.get_mut(&n) {
+            *count += 1;
+        } else {
+            numbers.insert(n, 1);
+        }
+    }
+
+    // Then check how many times list1's numbers appear in list2
+    list1.into_iter()
+        .map(|n| (n as usize) * numbers.get(&n).unwrap_or(&0))
         .sum()
 }
 
 fn main() {
     let (list1, list2) = read_input();
-    let n = match_numbers(&list1, &list2);
+    let n = calculate_similarity(list1, list2);
     println!("{n}");
 }
 
@@ -89,6 +66,13 @@ mod tests {
     fn test_simple_case() {
         let list1 = vec![3, 4, 2, 1, 3, 3];
         let list2 = vec![4, 3, 5, 3, 9, 3];
-        assert_eq!(match_numbers(&list1, &list2), 11);
+        assert_eq!(match_numbers(list1, list2), 11);
+    }
+
+    #[test]
+    fn test_similarity() {
+        let list1 = vec![3, 4, 2, 1, 3, 3];
+        let list2 = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(calculate_similarity(list1, list2), 31);
     }
 }
